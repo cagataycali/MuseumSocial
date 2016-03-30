e84145f075bd0371bdefa207f7f923076eabc386
Çağatay ÇALI <me@calicagatay.me>
2016-03-30 18:01:55 +0300

    Museum

---
+++ mobile-config.js
@@ -0,0 +1,36 @@
+/**
+ * Created by cagatay on 30/03/16.
+ */
+App.info({
+    id: 'com.ccali14.museumsocial',
+    name: 'Museum Social',
+    description: 'Get data from nearest beacon',
+    author: 'Cagatay CALI',
+    email: 'calicagatay@icloud.com',
+    website: 'http://cagataycali.xyz'
+});
+
+// Set up resources such as icons and launch screens.
+//App.icons({
+//    'iphone': 'icons/icon-60.png',
+//    'iphone_2x': 'icons/icon-60@2x.png',
+//    // ... more screen sizes and platforms ...
+//});
+//
+//App.launchScreens({
+//    'iphone': 'splash/Default~iphone.png',
+//    'iphone_2x': 'splash/Default@2x~iphone.png',
+//    // ... more screen sizes and platforms ...
+//});
+
+// Set PhoneGap/Cordova preferences
+//App.setPreference('BackgroundColor', '0xff0000ff');
+//App.setPreference('HideKeyboardFormAccessoryBar', true);
+//App.setPreference('Orientation', 'default');
+//App.setPreference('Orientation', 'all', 'ios');
+//
+//// Pass preferences for a particular PhoneGap/Cordova plugin
+//App.configurePlugin('com.phonegap.plugins.facebookconnect', {
+//    APP_ID: '1234567890',
+//    API_KEY: 'supersecretapikey'
+//});
